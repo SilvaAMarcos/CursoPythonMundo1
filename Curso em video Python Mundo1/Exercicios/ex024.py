d8ad0f894326d7856610a9eaf975ce8d052d2e84
SilvaAMarcos <marcos33112@gmail.com>
2022-06-09 22:37:23 -0300

    resolução ex024

---
+++ Curso em video Python Mundo1/Exercicios/ex024.py
@@ -0,0 +1,5 @@
+# checks if the first name of your city has SANTO
+city = str(input('Write your city her: ')).strip().upper().split()
+# city1 = city.upper().strip().split()
+# print('SANTO' in city1[0])
+print('SANTO' in city[0])
